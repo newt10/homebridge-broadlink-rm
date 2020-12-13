522c698e3c0c499eb4c09d58a08c60298cf50932
newt10 <mahavir25.dev@gmail.com>
2020-12-13 13:41:50 -0800

    Integrate platform helper

--- platform.js
+++ platform.js
@@ -1,4 +1,4 @@
-const { HomebridgePlatform } = require('homebridge-platform-helper');
+const { HomebridgePlatform } = require('./base');
 const { assert } = require('chai');
 
 const npmPackage = require('./package.json');
